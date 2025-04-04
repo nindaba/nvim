69a096db8baac004042e2eb20fad46ba38aa16ac
Arthur Nindaba <arthurninda@gmail.com>
2025-04-04 15:28:46 +0200

    Lazy vim custom

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- Leader key is set to space
+vim.g.mapleader = " "
+
+local map = vim.keymap
+
+map.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+map.set("n", "<leader>nl", ":nohl<CR>", { desc = "Clear search hightlights" })
+map.set("n", "\\", ":", { desc = "Toggle command mode" })
+map.set("n", "S", ":w<CR>", { desc = "Save buffer" })
