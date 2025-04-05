92b436b92564f9ccae80f1772ef985c766a9c5c2
Arthur Nindaba <arthurninda@gmail.com>
2025-04-05 15:19:40 +0200

    disabled dashboard

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -5,15 +5,15 @@ return {
     -- Simple configuration to attach to remote java debug process
     -- Taken directly from https://github.com/mfussenegger/nvim-dap/wiki/Java
     local dap = require("dap")
-    dap.configurations.java = {
-      {
-        type = "java",
-        request = "attach",
-        name = "Debug (Attach) - Remote",
-        hostName = "127.0.0.1",
-        port = 5005,
-      },
-    }
+    --dap.configurations.java = {
+    --  {
+    --  type = "java",
+    --request = "attach",
+    --name = "Debug (Attach) - Remote",
+    --hostName = "127.0.0.1",
+    --port = 5005,
+    --},
+    -- }
   end,
   dependencies = {
     {
---
+++ lua/plugins/icons.lua
@@ -0,0 +1,55 @@
+return {
+  { "echasnovski/mini.icons", enabled = false },
+  {
+    "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("nvim-web-devicons").setup({
+        override = {
+          zsh = {
+            icon = "",
+            color = "#2f2fd6",
+            cterm_color = "65",
+            name = "Zsh",
+          },
+        },
+        color_icons = true,
+        default = true,
+        strict = true,
+        variant = "light|dark",
+        override_by_filename = {
+          [".gitignore"] = {
+            icon = "",
+            color = "#f1502f",
+            name = "Gitignore",
+          },
+        },
+        override_by_extension = {
+          ["log"] = {
+            icon = "",
+            color = "#81e043",
+            name = "Log",
+          },
+          ["properties"] = {
+            icon = "",
+            color = "#7d8385",
+            cterm_color = "66",
+            name = "Properties",
+          },
+          ["sh"] = {
+            icon = "$",
+            color = "#2fd64b",
+            name = "Sh",
+          },
+        },
+        override_by_operating_system = {
+          ["apple"] = {
+            icon = "",
+            color = "#A2AAAD",
+            cterm_color = "248",
+            name = "Apple",
+          },
+        },
+      })
+    end,
+  },
+}
