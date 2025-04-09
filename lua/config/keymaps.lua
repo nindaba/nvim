073142a3031bf7bcd8d393f7db285580f21b0a8f
Arthur Nindaba <arthurninda@gmail.com>
2025-04-09 19:01:52 +0200

    resizing

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,10 +6,20 @@ vim.g.mapleader = " "
 
 local map = vim.keymap
 
-map.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+map.set("i", "jk", "<ESC>", { desc = "Exit insert mode" })
+map.set("i", "kj", "<ESC>", { desc = "Exit insert mode" })
 map.set("n", "<leader>nl", ":nohl<CR>", { desc = "Clear search hightlights" })
 map.set("n", "\\", ":", { desc = "Toggle command mode" })
 map.set("n", "S", ":w<CR>", { desc = "Save buffer" })
 map.set("t", "jk", "<C-\\><C-n>", { desc = "Escape insert mode" })
+map.set("t", "kj", "<C-\\><C-n>", { desc = "Escape insert mode" })
+
 -- Git
 map.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { desc = "Revert hunk" })
+
+-- Window
+map.set("n", "wj", ":res +10<CR>", { desc = "Resize window height down" })
+map.set("n", "<leader>j", "<C-W>-10", { desc = "Resize window height down" })
+map.set("n", "<leader>k", "<C-W>+10", { desc = "Resize window height up" })
+map.set("n", "<leader>h", "<C-W><10", { desc = "Resize window height left" })
+map.set("n", "<leader>l", "<C-W>>10", { desc = "Resize window height right" })
--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -205,12 +205,12 @@ return {
         cygwin_support = false,
       },
       diagnostics = {
-        enable = false,
+        enable = true,
         show_on_dirs = false,
         show_on_open_dirs = true,
         debounce_delay = 500,
         severity = {
-          min = vim.diagnostic.severity.HINT,
+          min = vim.diagnostic.severity.WARN,
           max = vim.diagnostic.severity.ERROR,
         },
         icons = {
@@ -221,7 +221,7 @@ return {
         },
       },
       modified = {
-        enable = false,
+        enable = true,
         show_on_dirs = true,
         show_on_open_dirs = true,
       },
